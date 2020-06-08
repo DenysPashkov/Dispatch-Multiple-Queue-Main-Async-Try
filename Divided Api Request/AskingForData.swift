c6b01b7b67b84504fe40770dea72a26a48d35158
dextronder97 <dexixoxa@yahoo.it>
2020-06-08 19:48:50 +0200

    first commit

---
+++ Divided Api Request/AskingForData.swift
@@ -0,0 +1,9 @@
+//
+//  AskingForData.swift
+//  Divided Api Request
+//
+//  Created by denys on 10/01/2020.
+//  Copyright © 2020 denys. All rights reserved.
+//
+
+import Foundation
---
+++ README.md
@@ -0,0 +1,1 @@
+# Dispatch-Multiple-Queue-Main-Async-Try
